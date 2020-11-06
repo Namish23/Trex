957378823ae5e7fa9e3e57a6c124262437ae3629
Namish23 <74053298+Namish23@users.noreply.github.com>
2020-11-06 17:36:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function setup() {
 
 function draw() {
   //trex.debug = true;
-  background("white");
+  background(rgb(153,255,255);
   text("Score: "+ score, 500,50);
   
   if (gameState===PLAY){
